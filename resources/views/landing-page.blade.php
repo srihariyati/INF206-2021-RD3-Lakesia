de89bb77e099df38838e408cf642d803651ac026
Ariq Yuda <ariqyuda@mhs.unsyiah.ac.id>
2021-05-06 01:30:02 +0700

    Menambahkan landing page

---
+++ resources/views/landing-page.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="/css/LandingPageStyle.css">
+    </head>
+
+    <body>
+        <div class="lakesia-text">Lakesia</div>
+        <div class="lakesia-text1">Layanan Kesehatan Lansia</div>
+        <div> <img class="lakesia-logo" src="/img/Lakesia-Logo.png" alt="Lakesia Logo"> </div>
+        <div class="text1">Memudahkan anda untuk pengontrolan kesehatan</div>
+        <div class="text2">Wilayah Kopelma Darussalam</div>
+        <div> <img class="medical-logo" src="/img/Medical-Logo.png" alt="Medical Logo"> </div>
+        <div> <img class="profile-logo" src="/img/Profile-Logo.png" alt="Profile Logo"> </div>
+        
+        <form method="post">
+            <table class="login">
+                <tr>
+                    <td>Username <br> <input type="text" name="user" placeholder="Enter Username" name="uname" required> </td>
+                </tr>
+                <tr>
+                    <td>Password <br> <input type="password" name="pass" placeholder="Enter Password" name="pass" required> </td>
+                </tr>
+                <tr>
+                    <td>
+                        Login sebagai: <select name="usertype">
+                            <option value="lansia">Lansia</option>
+                            <option value="perawat">Perawat</option>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td><button type="submit">Login</button></td>
+                </tr>
+            </table>
+        </form>
+        
+        <div class="text3">Belum punya akun?</div>
+        <div class="text4">Daftar disini</div>
+        <div class="foot"></div>
+        <div class="text5">© 2021 Made by Tim RD 3</div>
+
+    </body>
+
+    
+
+</html>
