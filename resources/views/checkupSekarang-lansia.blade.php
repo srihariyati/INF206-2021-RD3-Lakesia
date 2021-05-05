d77d59f176977a162eeff600a885357e11711723
sri hariyati <sri.hr@mhs.unsyiah.ac.id>
2021-05-06 01:51:28 +0700

    Menambahkan beberapa halaman

---
+++ resources/views/checkupSekarang-lansia.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="\css\CheckUpSekarangStyle.css">
+        <script src="\js\CheckUpSekarang.js"></script>
+    </head>
+
+    <body>
+    
+        
+    <div> <img class="Lakesia-Logo" src="\img\Logo.png" alt="Lakesia Logo"> </div>
+    
+    <div class='CheckUpSekarang-text'>Check Up Sekarang </div>
+    <img class="List-Kontak1" src="\img\Rectangle-InfoKontak.png"/>
+    <img class="List-Kontak2" src="\img\Rectangle-InfoKontak.png"/>
+    <img class="List-Kontak3" src="\img\Rectangle-InfoKontak.png"/>
+
+    <div class="Button InfoKontak1">Info kontak</div>
+    <div class="Button InfoKontak2">Info kontak</div>
+    <div class="Button InfoKontak3">Info kontak</div>
+
+     <a onclick="goBack()" class="previous round">&#8592;</a>
+    
+  
+    </body>
+
+</html>
