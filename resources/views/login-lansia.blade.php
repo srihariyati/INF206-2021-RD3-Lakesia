6e5f084d220f0369cb5d95f977d5d96f4c4340ae
samuel <gloryarossana@gmail.com>
2021-05-19 21:33:08 +0700

    mengupdate file DaftarLansia dan login-lansia
    menambahkan dan mengupdate jenis form

--- resources/views/login-lansia.blade.php
+++ resources/views/login-lansia.blade.php
@@ -1,12 +1,50 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Login lansia</h1>
-</body>
+
+<html>
+
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="/css/LoginPageStyle.css">
+    </head>
+
+    <body>
+        <div class="lakesia-text">Lakesia</div>
+        <div class="lakesia-text1">Layanan Kesehatan Lansia</div>
+        <div> <img class="lakesia-logo" src="/img/Lakesia-Logo.png" alt="Lakesia Logo"> </div>
+        <div class="text1">Memudahkan anda untuk pengontrolan kesehatan</div>
+        <div class="text2">Wilayah Kopelma Darussalam</div>
+        <div> <img class="medical-logo" src="/img/Medical-Logo.png" alt="Medical Logo"> </div>
+        <div> <img class="profile-logo" src="/img/Profile-Logo.png" alt="Profile Logo"> </div>
+        
+        <div >
+
+        <form method="post" action= "">
+            <table class="login">
+                <label class="textlogin">Akun Lansia</label>
+                <tr>
+                    <td>Username <br> <input type="text" name="user" placeholder="Enter Username" name="uname" required> </td>
+                </tr>
+                <tr>
+                    <td>Password <br> <input type="password" name="pass" placeholder="Enter Password" name="pass" required> </td>
+                </tr>
+                <tr>
+                    <td><button type="submit">Login</button></td>
+                </tr>
+            </table>
+        </form>
+        
+        <div class= "text3">Belum punya akun? </div>
+        <div class="daftar">
+            <a href=#box>Daftar disini</a>.</div>
+        <div class="foot"></div>
+        <div class="text5">© 2021 Made by Tim RD 3</div>
+
+        
+           <div id="box">
+               Daftar sebagai <br><br>
+               <a href=daftar/lansia class="lansia"> Lansia </a><br>
+               <a href=daftar/perawat class="perawat"> Perawat </a><br>
+               <a href=# class="batal"> x </a>
+            </div>
+        
 </html>
